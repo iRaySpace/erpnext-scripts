b09ca2dcc91b5917d6f7248e6f6165565b589b24
Ivan Ray Altomera <irayspacii@gmail.com>
2018-09-12 15:04:15 +0800

    update script docfield

--- script_10.py
+++ script_10.py
@@ -1,8 +1,17 @@
 import frappe
+import csv
+
+from frappe import get_site_path
 
 
 def rearrange_fields(**kwargs):
-    print "======================"
-    print kwargs['doctype']
-    print kwargs['file']
-    print "======================"
+    """Using the file uploaded, update the DocField idx for rearrangement"""
+    file_path = get_site_path('public', 'files', kwargs['file'])
+
+    with open(file_path) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            index = row[0]
+            field = row[1]
+            frappe.db.sql("""UPDATE `tabDocField` SET idx={0} WHERE fieldname='{1}' AND parent='{2}'""".format(index, field, kwargs['doctype']))
+        frappe.db.commit()

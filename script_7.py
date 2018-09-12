cda1c86d745da0b52103a42ba1d618663338fc53
Ivan Ray Altomera <irayspacii@gmail.com>
2018-09-12 11:31:48 +0800

    fix unknown 's'

--- script_7.py
+++ script_7.py
@@ -8,7 +8,7 @@ def set_field_included_in_listview(*args, **kwargs):
     d = frappe.get_doc("Customize Form")
     d.doc_type = kwargs['doc_type']
     d.run_method("fetch_to_customize")
-s
+
     for field in args:
         # Get the field and set to be included in the list view
         d.get("fields", {'fieldname': kwargs['field']})[0].in_list_view = 1

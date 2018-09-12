54544b715005c8ab6f17a2d9715c98b6f0c0a2b6
Ivan Ray Altomera <irayspacii@gmail.com>
2018-09-12 11:28:45 +0800

    update script for field in list view

--- script_7.py
+++ script_7.py
@@ -1,10 +1,17 @@
 import frappe
 
 
-def set_field_included_in_listview(**kwargs):
+def set_field_included_in_listview(*args, **kwargs):
     """Set field to be included in the listview"""
 
-    print "================"
-    print kwargs['field']
-    print kwargs['doc_type']
-    print "================"
+    # Get the form
+    d = frappe.get_doc("Customize Form")
+    d.doc_type = kwargs['doc_type']
+    d.run_method("fetch_to_customize")
+s
+    for field in args:
+        # Get the field and set to be included in the list view
+        d.get("fields", {'fieldname': kwargs['field']})[0].in_list_view = 1
+
+    # Save the form
+    d.run_method("save_customization")

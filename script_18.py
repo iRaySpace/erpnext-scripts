b04449c94025ff5ad45eb4389268a84a35d3ba7b
Ivan Ray Altomera <irayspacii@gmail.com>
2018-10-12 14:30:40 +0800

    Update for set customer name as name

---
+++ script_18.py
@@ -0,0 +1,15 @@
+import frappe
+
+
+def set_customer_name_as_name():
+    """Gets the customer name and set the document name"""
+    customers = frappe.get_all('Customer')
+
+    for customer in customers:
+        print '[*] Updating name for the customer {0}'.format(customer.name)
+
+        customer_name = frappe.db.get_value('Customer', customer.name, 'customer_name')
+        frappe.db.sql("""UPDATE `tabCustomer` SET name=%s""", customer_name)
+        frappe.db.commit()
+
+    print '[/] Finished processing'

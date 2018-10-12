8e4e8ef4da597a464fb5faa3c9983a03df169d22
Ivan Ray Altomera <irayspacii@gmail.com>
2018-10-12 14:40:12 +0800

    Fix problem

--- script_18.py
+++ script_18.py
@@ -9,7 +9,7 @@ def set_customer_name_as_name():
         print '[*] Updating name for the customer {0}'.format(customer.name)
 
         customer_name = frappe.db.get_value('Customer', customer.name, 'customer_name')
-        frappe.db.sql("""UPDATE `tabCustomer` SET name=%s""", customer_name)
+        frappe.db.sql("""UPDATE `tabCustomer` SET name=%s WHERE customer_name=%s""", (customer_name, customer_name))
         frappe.db.commit()
 
     print '[/] Finished processing'

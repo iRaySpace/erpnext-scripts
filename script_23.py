51155aeecee1abe19c361e2973175b4e915e0354
Ivan Ray Altomera <irayspacii@gmail.com>
2018-10-13 12:30:39 +0800

    Set user role profile

---
+++ script_23.py
@@ -0,0 +1,12 @@
+import frappe
+
+
+def set_user_role_profile(user, role_profile):
+    """Set user role profile"""
+    try:
+        user = frappe.get_doc('User', user)
+        user.role_profile_name = role_profile
+        user.save()
+        print '[*] User {0} set as {1}'.format(user, role_profile)
+    except:
+        print '[x] Unable to set role profile {0} to user {1}'.format(role_profile, user)

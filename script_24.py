659c4df10839c57cdc43595b61ce940efa1bd684
Ivan Ray Altomera <irayspacii@gmail.com>
2018-10-16 11:09:54 +0800

    update send_email

--- script_24.py
+++ script_24.py
@@ -4,4 +4,13 @@ import frappe
 def send_email(**kwargs):
     """Create email"""
     email = kwargs['email']
-    frappe.sendmail(recipients=[email], subject='Test', message='When you are gone', delayed=False)
+    attachments = [{
+        "lang": "en",
+        "print_format": "Standard",
+        "doctype": "Sales Order",
+        "print_letterhead": 1,
+        "html": "",
+        "print_format_attachment": 1,
+        "name": "SO-00222"
+    }]
+    frappe.sendmail(recipients=[email], subject='Test', message='When you are gone', delayed=False, attachments=attachments)

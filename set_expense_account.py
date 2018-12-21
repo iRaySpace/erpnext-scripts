64864607cf70e1f6e400deeaf1361c2f3c498aa1
Ivan Ray Altomera <irayspacii@gmail.com>
2018-12-21 16:21:57 +0800

    Added new function

--- set_expense_account.py
+++ set_expense_account.py
@@ -9,3 +9,14 @@ def set_items(**kwargs):
         print 'Setting up {0}/{1} items.'.format(i + 1, item_length)
         frappe.db.sql("""UPDATE `tabItem` SET expense_account=%s WHERE name=%s""", (kwargs['expense_account'], item.name))
         frappe.db.commit()
+
+
+def set_invoice_items(**kwargs):
+    items = frappe.db.sql("""SELECT DISTINCT item_code FROM `tabSales Invoice Item`""", as_dict=True)
+
+    max_items = len(items)
+
+    for i, item in enumerate(items):
+        print 'Updating {0}/{1}'.format(i + 1, max_items)
+        account = frappe.db.sql("""SELECT expense_account FROM `tabItem` WHERE name=%s""", item.item_code, as_dict=1)[0]
+        frappe.db.sql("""UPDATE `tabSales Invoice Item` SET expense_account=%s WHERE item_code=%s""", (account.expense_account, item.item_code))

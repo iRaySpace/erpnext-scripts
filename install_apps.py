cddb3c083bd46026cdf05a859e26ed5c6aeb2c8b
Ivan Ray Altomera <irayspacii@gmail.com>
2019-01-30 11:36:27 +0800

    Install app

---
+++ install_apps.py
@@ -0,0 +1,27 @@
+import frappe
+
+from frappe.installer import install_app as _install_app
+
+
+def execute(**kwargs):
+    """
+    Use this to install apps; it checks for apps that are already installed.
+    bench --site [site_name] execute [module_name].install_apps.execute
+        --kwargs "{'apps': ['rebrandly_integration', 'printnode_integration']}"
+    """
+    apps_installed = frappe.db.sql("""SELECT DISTINCT app_name FROM `tabModule Def`""", as_dict=True)
+    apps_installed = [v.app_name for v in apps_installed]
+
+    for app in kwargs['apps']:
+        if app not in apps_installed:
+            print 'Installing app {0}'.format(app)
+
+            frappe.init(site=frappe.local.site)
+            frappe.connect()
+
+            try:
+                _install_app(app, verbose=False)
+            finally:
+                frappe.destroy()
+
+
